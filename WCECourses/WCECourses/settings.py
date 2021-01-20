d410615b39fd38be0f31e4bbbcc1076fcaed1122
KshitijSabale <kshitijsabale14@gmail.com>
2021-01-20 12:49:30 +0530

    static|student|faculty

--- WCECourses/WCECourses/settings.py
+++ WCECourses/WCECourses/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'student',
+    'faculty',
 ]
 
 MIDDLEWARE = [
@@ -117,4 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
